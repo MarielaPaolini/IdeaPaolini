cc60b4302b72802996e72b847cd78256373f5ef8
Mariela Paolini <marielapaolini27@gmail.com>
2022-08-22 20:00:50 -0300

    Realizo NavBar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,29 @@
+import logo from '../../logo.svg';
+import './styles.css'
+const NavBar = () => {
+    return (
+        <div className='navbar-container'>
+            <div>
+                <img width={'100px'} src={logo} alt="logo"/>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <a href=''>Inicio</a>
+                    </li>
+                    <li>
+                        <a href=''>Nosotros</a>
+                    </li>
+                    <li>
+                        <a href=''>Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </div>
+    )
+};
+
+export default NavBar;
